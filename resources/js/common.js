193b91e4fb3ac6bee09821438c521827c2ac4a05
yanglae <yanglae@hanmail.net>
2021-10-20 17:47:01 +0900

    모바일 수정

--- resources/js/common.js
+++ resources/js/common.js
@@ -82,81 +82,83 @@ function gnbMenu(depth1, depth2, depth3) {
 	  });
 
 
-//////////// 전체메뉴 햄버거메뉴 ////////////
-// 모바일 - 전체내용 오픈
-$moGnbWrap.hide();
-$moBtnOpen.on("click", function(e) {
-  e.preventDefault();
-  $(this).hide();
-    
-  //$("body").addClass('fixed');
-  $moGnbWrap.fadeIn(200);
-  $moGnbWrap.find(".scroll").stop().animate({right:0}, 300);        
-  $moGnbBg.fadeIn();
-  $("body").css({'height':$(window).height(), 'overflow':'hidden'});
-});
+    //////////// 전체 햄버거 메뉴 ////////////
+    // 메뉴열기
+    $moGnbWrap.hide();
+    $moBtnOpen.on("click", function(e) {
+      e.preventDefault();
 
-$moBtnClose.on("click", function(e) {
-  e.preventDefault();
-    
-  //$("body").removeClass('fixed');
-  $moBtnOpen.show();
-  $moGnbWrap.fadeOut(200);
-  $moGnbWrap.find(".scroll").stop().animate({right:-100}, 300);
-  $moGnbBg.hide();
-  $("body").css({'height':'auto', 'overflow':'auto'});
-});	
-    
-$moGnbBg.on("click", function(e) {
-  e.preventDefault();
-    
-  $moGnbWrap.stop().fadeOut(200);        
-  $moGnbWrap.find(".scroll").animate({right:0}, 300);
-  $moBtnOpen.show();  
-});
+      $(this).hide();
+      $moGnbWrap.fadeIn(200);
+      //$moGnbWrap.find(".scroll").stop().animate({right:0}, 300);
+      $moGnbWrap.find(".scroll").addClass("open");
+      $moGnbBg.fadeIn();
+      $("body").css({'height':$(window).height(), 'overflow':'hidden'});
+    });
 
-// 모바일 - 1,2DEPTH 오픈
-$moGnbLi.children("a").on("click", function(e) {
-//$moGnbLi.find('> a').on('click', function(e) { //2차뎁스만 사용시
-  e.preventDefault();
-    
-    var $depth = $(this).next("ul");
-    if($depth.is(":visible")){        
-        $(this).removeClass("on");
-        $depth.slideUp(300);
-    } else {
-      $moGnbLi.children("a").removeClass("on");
-        //$moGnb.find("li > a").removeClass("on");
-        $(this).parent().siblings().find("ul").slideUp(300);            
-        $(this).addClass("on");
-        $depth.slideDown(300);
+    // 메뉴닫기
+    $moBtnClose.on("click", function(e) {
+      e.preventDefault();
+      
+      $moBtnOpen.show();
+      $moGnbWrap.fadeOut(200);
+      //$moGnbWrap.find(".scroll").stop().animate({right:-100}, 300);
+      $moGnbWrap.find(".scroll").removeClass("open");
+      $moGnbBg.hide();
+      $("body").css({'height':'auto', 'overflow':'auto'});
+    });	
+
+    $moGnbBg.on("click", function(e) {
+      e.preventDefault();
+      
+      $moBtnOpen.show();
+      $moGnbWrap.fadeOut(200);
+      //$moGnbWrap.find(".scroll").animate({right:0}, 300);
+      $moGnbWrap.find(".scroll").removeClass("open");
+    });
+
+    // 모바일 - 1,2DEPTH 오픈
+    $moGnbLi.children("a").on("click", function(e) {
+    //$moGnbLi.find('> a').on('click', function(e) { //2차뎁스만 사용시
+      e.preventDefault();
+        
+        var $depth = $(this).next("ul");
+        if($depth.is(":visible")){
+            $(this).removeClass("on");
+            $depth.slideUp(300);
+        } else {
+          $moGnbLi.children("a").removeClass("on");
+            //$moGnb.find("li > a").removeClass("on");
+            $(this).parent().siblings().find("ul").slideUp(300);
+            $(this).addClass("on");
+            $depth.slideDown(300);
+        }
+    });
+
+    $(".moGnb .depth1 > li").children("a").on('click', function() {
+        var $depth = $(this).next("ul");
+        if($depth.is(":visible")){
+          $(this).removeClass("on");
+            $depth.slideUp(300);
+        } else {
+          $(".moGnb .depth1 > li").children("a").removeClass("on");
+            $(this).parent().siblings().find("ul").slideUp(300);
+            $(this).addClass("on");
+            $depth.slideDown(300);
+        }
+    });
+
+    //모바일 - 페이지 인식
+    if ($moGnbLi.length > depth1-1) {
+        $moGnbLi.eq(depth1-1).find("> a").addClass("on");
+        $moGnbLi.eq(depth1-1).find(".depth1 > li").eq(depth2-1).find("> a").addClass("on");
+        $moGnbLi.eq(depth1-1).find(".depth1 > li").eq(depth2-1).find(".depth2 > li").eq(depth3-1).find("> a").addClass("on");
     }
-});
 
-$(".moGnb .depth1 > li").children("a").on('click', function() {
-    var $depth = $(this).next("ul");
-    if($depth.is(":visible")){
-      $(this).removeClass("on");
-        $depth.slideUp(300);
-    } else {
-      $(".moGnb .depth1 > li").children("a").removeClass("on");
-        $(this).parent().siblings().find("ul").slideUp(300);
-        $(this).addClass("on");
-        $depth.slideDown(300);
+    //모바일 - 2depth 오픈
+    if ( depth1 >= 0 && depth2 >= 0 ) {
+        $moGnbLi.eq(depth1-1).addClass("on").children(".depth1").show().children("li").eq(depth2-1).addClass("on").children(".depth2").show();
     }
-});
-
-//모바일 - 페이지 인식
-if ($moGnbLi.length > depth1-1) {
-    $moGnbLi.eq(depth1-1).find("> a").addClass("on");
-    $moGnbLi.eq(depth1-1).find(".depth1 > li").eq(depth2-1).find("> a").addClass("on");
-    $moGnbLi.eq(depth1-1).find(".depth1 > li").eq(depth2-1).find(".depth2 > li").eq(depth3-1).find("> a").addClass("on");
-}
-
-//모바일 - 2depth 오픈
-if ( depth1 >= 0 && depth2 >= 0 ) {
-    $moGnbLi.eq(depth1-1).addClass("on").children(".depth1").show().children("li").eq(depth2-1).addClass("on").children(".depth2").show();
-}
 
 }
 
@@ -196,7 +198,7 @@ $(function() {
       },
   
       pagination: {
-        el: '.swiper-pagination',      
+        el: '.swiper-pagination',
         type: 'fraction',
       },
       navigation: {
